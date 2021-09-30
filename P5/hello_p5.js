d1d77a6fd4d93cfd455ddb785a4d5cb396bf0784
lilyjohnson23 <lilyjohnson23@marlborough.org>
2021-09-30 09:04:06 -0700

    Update hello_p5.js

--- P5/hello_p5.js
+++ P5/hello_p5.js
@@ -1,15 +1,15 @@
 function setup() {
   createCanvas(400, 400);
+  background(0);
 }
 
 let x = 50;
 
 function draw() {
-  background(0);
-  fill(255,0,0);
-  ellipse(x, 200, 100, 50);
-  x++;
 
-  if (x > width+50)
-    x = -50;
+}
+
+function mousePressed(){
+fill(random(255),random(255),random(255));
+ellipse(mouseX,mouseY,30,30);
 }
